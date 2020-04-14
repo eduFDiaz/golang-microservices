7a03dede4bc7db10e2349106262aa9b46fc915cf
Eduardo A Fernandez Diaz <fernandez9000@gmail.com>
2020-04-13 21:47:39 -0500

    unit and integration testing

---
+++ mvc/domain/user_dao_test.go
@@ -0,0 +1,55 @@
+package domain
+
+import (
+	"fmt"
+	"net/http"
+	"reflect"
+	"testing"
+
+	"github.com/golang-microservices/mvc/utils"
+)
+
+func TestGetUser(t *testing.T) {
+	type args struct {
+		userID int64
+	}
+	tests := []struct {
+		name  string
+		args  args
+		want  *User
+		want1 *utils.ApplicationError
+	}{
+		{
+			name: "Test Get user id with 1",
+			args: args{int64(1)},
+			want: &User{
+				ID:        1,
+				FirstName: "Eduardo",
+				LastName:  "Fernandez",
+				Email:     "fenandez9000@gmail.com",
+			},
+			want1: nil,
+		},
+		{
+			name: "Test Get user id with 123",
+			args: args{int64(123)},
+			want: nil,
+			want1: &utils.ApplicationError{
+				Message:    fmt.Sprintf("User with id = %v not found", int64(123)),
+				StatusCode: http.StatusNotFound,
+				Code:       "not found",
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, got1 := GetUser(tt.args.userID)
+			if !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("GetUser() got = %v, want %v", got, tt.want)
+			}
+			if !reflect.DeepEqual(got1, tt.want1) {
+				t.Errorf("GetUser() got1 = %v, want %v", got1, tt.want1)
+			}
+		})
+	}
+}
---
+++ mvc/services/users_service_test.go
@@ -0,0 +1,56 @@
+package services
+
+import (
+	"fmt"
+	"net/http"
+	"reflect"
+	"testing"
+
+	"github.com/golang-microservices/mvc/domain"
+	"github.com/golang-microservices/mvc/utils"
+)
+
+func TestGetUser(t *testing.T) {
+	type args struct {
+		userId int64
+	}
+	tests := []struct {
+		name  string
+		args  args
+		want  *domain.User
+		want1 *utils.ApplicationError
+	}{
+		{
+			name: "Test Get user id with 1",
+			args: args{int64(1)},
+			want: &domain.User{
+				ID:        1,
+				FirstName: "Eduardo",
+				LastName:  "Fernandez",
+				Email:     "fenandez9000@gmail.com",
+			},
+			want1: nil,
+		},
+		{
+			name: "Test Get user id with 123",
+			args: args{int64(123)},
+			want: nil,
+			want1: &utils.ApplicationError{
+				Message:    fmt.Sprintf("User with id = %v not found", int64(123)),
+				StatusCode: http.StatusNotFound,
+				Code:       "not found",
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, got1 := GetUser(tt.args.userId)
+			if !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("GetUser() got = %v, want %v", got, tt.want)
+			}
+			if !reflect.DeepEqual(got1, tt.want1) {
+				t.Errorf("GetUser() got1 = %v, want %v", got1, tt.want1)
+			}
+		})
+	}
+}
